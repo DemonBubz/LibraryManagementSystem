4743ad4727c836089e6e14680f91377b1cedf189
Apophis_ <ancientphoebus@gmail.com>
2021-05-22 15:04:09 +0530

    add serializeUser() and deserializeUser() fix multiple password prompts bug rework checkPass() and returnIfFound() functions rework id to random uuid

--- src/libProj/User.java
+++ src/libProj/User.java
@@ -1,52 +1,55 @@
 package libProj;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.UUID;
 
-public class User {
+public class User implements Serializable{
     ArrayList<User>userArrayList=new ArrayList<>();
     boolean haveIssued=false;
-    Scanner scanner=new Scanner(System.in);
-    static int userIdGenerator=0;
-    int userId;
+    UUID userId;
     String password,role,name;
     User(){}
     User(String name,String role,String password){
         System.out.println("generating user Id");
-        userIdGenerator++;
-        setUserId(userIdGenerator);
+        this.userId=UUID.randomUUID();
         this.role=role;
         this.password=password;
         this.name=name;
     }
-    public void createAdmin(){
-        System.out.println("Enter name");
+    public void createAdmin() throws IOException{
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter name: ");
         String name=scanner.nextLine();
-        System.out.println("Enter password");
+        System.out.print("Enter password: ");
         String pass=scanner.nextLine();
         userArrayList.add(new User(name,"Admin",pass));
+        serializeUser();
     }
-    public void addUser(){
+    public void addUser() throws IOException {
+        Scanner scanner=new Scanner(System.in);
         System.out.println("Generating Id for user..");
 
-        System.out.println("Enter name");
+        System.out.print("Enter name: ");
         setName(scanner.nextLine());
         String currName=getName();
 
-        System.out.println("Enter password");
+        System.out.print("Enter password: ");
         setPassword(scanner.nextLine());
         String currPass=getPassword();
 
         userArrayList.add(new User(currName,"User",currPass));
+        serializeUser();
     }
     public void displayUsers(){
         for(int i=0;i<userArrayList.size();i++){
             User user=userArrayList.get(i);
-            System.out.println(user.userId+" "+user.name+" "+user.role+" "+user.haveIssued);
+            if(user.haveIssued) {
+                System.out.println(user.userId + " " + user.name + " " + user.role + " " + "Book Issued");
+            }else System.out.println(user.userId+" "+user.name+" "+user.role+" "+"Book not Issued");
+
         }
-    }
-    public void setUserId(int userId) {
-        this.userId = userId;
     }
     public String getPassword() {
         return password;
@@ -60,7 +63,7 @@ public class User {
     public void setName(String name) {
         this.name = name;
     }
-    public int getUserId() {
+    public UUID getUserId() {
         return userId;
     }
     public boolean isAdminAndCheckPass(String name){
@@ -71,11 +74,15 @@ public class User {
             Admin=userArrayList.get(i);
             if(name.equals(Admin.name))
                 isPresent=true;
-            if(isPresent && Admin.role.equalsIgnoreCase("Admin")){
-                System.out.println("Enter password");
-                String pass=scanner.nextLine();
-                if(pass.equals(Admin.password)){
-                    AdminLogged=true;
+            if(isPresent ){
+                if(Admin.role.equals("Admin")) {
+                    System.out.print("Enter password: ");
+                    String pass = scanner.nextLine();
+                    System.out.println();
+                    if (pass.equals(Admin.password)) {
+                        AdminLogged = true;
+                        break;
+                    }
                 }
             }
         }
@@ -83,23 +90,24 @@ public class User {
         return AdminLogged;
     }
     public User returnIfFound(){
-        System.out.println("Enter Name");
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter Name: ");
         String name=scanner.nextLine();
         User temp2=new User();
-        boolean isPresentAdmin=isAdminAndCheckPass(name);
-        boolean isPresentUser=isUserAndCheckPass(name);
-        if(isPresentAdmin || isPresentUser){
+        boolean isPresentUser=isPresentAndCheckPass(name);
+        if(isPresentUser){
             for(int i=0;i<userArrayList.size();i++){
                 User temp= userArrayList.get(i);
-                if(temp.getName().equalsIgnoreCase(name))
-                    temp2=temp;
-                return temp2;
+                if(temp.getName().equalsIgnoreCase(name)) {
+                    temp2 = temp;
+                    return temp2;
+                }
             }
         }
         return temp2;
     }
 
-    public boolean isUserAndCheckPass(String name){
+    public boolean isPresentAndCheckPass(String name){
         boolean isPresent=false,userLogged=false;
         Scanner scanner=new Scanner(System.in);
         User user;
@@ -107,17 +115,31 @@ public class User {
             user=userArrayList.get(i);
             if(name.equalsIgnoreCase(user.name))
                 isPresent=true;
-            if(isPresent && user.role.equalsIgnoreCase("User")){
-                System.out.println("Enter password");
-                String pass=scanner.nextLine();
-                if(pass.equals(user.password)){
-                    userLogged=true;
-                }
+            if(isPresent ){
+                    System.out.print("Enter password: ");
+                    String pass = scanner.nextLine();
+                    if (pass.equals(user.password)) {
+                        userLogged = true;
+                        break;
+                    }
             }
         }
 
         return userLogged;
     }
-
+    public void serializeUser() throws IOException{
+            FileOutputStream file = new FileOutputStream("user.ser");
+            ObjectOutputStream userObject=new ObjectOutputStream(file);
+            userObject.writeObject(userArrayList);
+            userObject.close();
+            file.close();
+    }
+    public void deserializeUser() throws IOException,ClassNotFoundException {
+            FileInputStream file = new FileInputStream("user.ser");
+            ObjectInputStream bookObject=new ObjectInputStream(file);
+            ArrayList<User>userListFromFile=(ArrayList<User>)bookObject.readObject();
+            bookObject.close();
+            userArrayList=userListFromFile;
+    }
 }
 
